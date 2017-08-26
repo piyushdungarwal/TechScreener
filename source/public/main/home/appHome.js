3567f90789d37fe5f9822156282154ee5666ba7c
payalb <botharapayal@gmail.com>
2017-08-26 11:02:39 +0530

    Removing old files

--- source/public/main/home/appHome.js
+++
@@ -1,5 +0,0 @@
-import appHomeComponent from './appHome.component';
-
-var appHomeModule = angular.module('appHomeModule', []).component('appHome', appHomeComponent);
-
-export default appHomeModule;
